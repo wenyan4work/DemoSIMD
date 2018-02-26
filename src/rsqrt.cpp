6907b8c99a7269d97dd494cbb10e3b40854304ee
wenyan4work <wenyan4work@gmail.com>
2018-02-26 09:13:59 -0500

    add rsqrt_ps avx

--- src/rsqrt.cpp
+++ src/rsqrt.cpp
@@ -4,6 +4,8 @@
 #include <cstdlib>
 #include <cstring>
 
+#include <immintrin.h>
+
 #include "AlignedMemory.hpp"
 #include "Timer.hpp"
 
@@ -58,6 +60,36 @@ void test_simple(int size) {
     printf("result0: %lf; Speed: %lf MRSQRT/s\n", result[0], (size * 8) * (1e-6) / timer.getTime());
 }
 
+void test_avxf(int size) {
+    AlignedMemory<float, 32> datamem(8);
+    AlignedMemory<float, 32> resultmem(8);
+
+    float *data = datamem.alignedPtr;
+    float *result = resultmem.alignedPtr;
+
+    for (int i = 0; i < 8; i++) {
+        data[i] = i + 1;
+        result[i] = 0;
+    }
+    // float data[8] = {1, 2, 3, 4, 5, 6, 7, 8};
+    // float result[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+
+    Timer timer;
+    timer.start();
+
+    const __m256 d = _mm256_load_ps(data);
+    for (unsigned int j = 0; j < size; j++) {
+        __m256 r = _mm256_load_ps(result);
+        __m256 rsqrt = _mm256_rsqrt_ps(_mm256_add_ps(r, d)); // fast inv sqrt
+        r = _mm256_add_ps(r, rsqrt);
+        _mm256_store_ps(result, r);
+    }
+
+    timer.stop("simple rsqrt complete, ");
+    timer.dump();
+    printf("result0: %lf; Speed: %lf MRSQRT/s\n", result[0], (size * 8) * (1e-6) / timer.getTime());
+}
+
 void test_magicf(int size) {
 
     double data[8] = {1, 2, 3, 4, 5, 6, 7, 8};
@@ -95,8 +127,9 @@ void test_magicd(int size) {
 void test_ompsimd(int size) {}
 
 int main() {
-    test_simple(100000);
-    test_magicf(100000);
-    test_magicd(100000);
+    test_simple(1000000);
+    test_magicf(1000000);
+    test_magicd(1000000);
+    test_avxf(1000000);
     return 0;
 }
