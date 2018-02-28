4e5efa65fea3c15b7c6828fb8877c04cb953290b
wenyan4work <wenyan4work@gmail.com>
2018-02-28 08:25:41 -0500

    fix avxd nwtn interation bug, and adjust output format.

--- src/rsqrt.cpp
+++ src/rsqrt.cpp
@@ -1,10 +1,16 @@
 #include <cmath>
 #include <cstdio>
-
 #include <immintrin.h>
+#include <iomanip>
+#include <iostream>
+#include <limits>
 
 #include "AlignedMemory.hpp"
 #include "Timer.hpp"
+
+/***********************
+ *   Double Precision  *
+ ***********************/
 
 inline double frsqrtd(double number) {
     union {
@@ -23,23 +29,6 @@ inline double frsqrtd(double number) {
     // Newton iteration: rinv = 0.5 rinv_approx ( 3 - r2 rinv_approx^2 )
 
     return conv.d;
-}
-
-inline float frsqrtf(float number) {
-    union {
-        float f;
-        long i;
-    } conv;
-
-    float x2;
-    const float threehalfs = 1.5F;
-
-    x2 = number * 0.5F;
-    conv.f = number;
-    conv.i = 0x5f3759df - (conv.i >> 1); // magic number for float
-    conv.f = conv.f * (threehalfs - (x2 * conv.f * conv.f));
-    conv.f = conv.f * (threehalfs - (x2 * conv.f * conv.f));
-    return conv.f;
 }
 
 void test_simple(int size) {
@@ -61,7 +50,159 @@ void test_simple(int size) {
     for (int i = 0; i < 8; i++) {
         sum += result[i];
     }
-    printf("result0: %.17g; Speed: %lf MRSQRT/s\n", sum, (size * 8) * (1e-6) / timer.getTime());
+    std::cout << "result0: " << std::setprecision(std::numeric_limits<double>::digits10 + 2) << sum << ", ";
+    printf("Speed: %lf MRSQRT/s\n", (size * 8) * (1e-6) / timer.getTime());
+}
+
+void test_magicd(int size) {
+
+    double data[8] = {1, 2, 3, 4, 5, 6, 7, 8};
+    double result[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+
+    Timer timer;
+    timer.start();
+    for (int j = 0; j < size; j++) {
+        for (int i = 0; i < 8; i++) {
+            result[i] += frsqrtd(data[i] + result[i]);
+        }
+    }
+    timer.stop("DOUBLE magic rsqrt complete, ");
+    timer.dump();
+    double sum = 0;
+    for (int i = 0; i < 8; i++) {
+        sum += result[i];
+    }
+    std::cout << "result0: " << std::setprecision(std::numeric_limits<double>::digits10 + 2) << sum << ", ";
+    printf("Speed: %lf MRSQRT/s\n", (size * 8) * (1e-6) / timer.getTime());
+}
+
+
+void test_avxd1(int size) {
+    AlignedMemory<double, 32> datamem(8);
+    AlignedMemory<double, 32> resultmem(8);
+
+    double *data = datamem.alignedPtr;
+    double *result = resultmem.alignedPtr;
+
+    for (int i = 0; i < 8; i++) {
+        data[i] = i + 1;
+        result[i] = 0;
+    }
+    // float data[8] = {1, 2, 3, 4, 5, 6, 7, 8};
+    // float result[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+
+    Timer timer;
+    timer.start();
+
+    const __m256d d0 = _mm256_load_pd(data);
+    const __m256d d1 = _mm256_load_pd(data + 4);
+    const __m256d half = _mm256_set1_pd(0.5);
+    const __m256d three = _mm256_set1_pd(3.0);
+
+    __m256d r0 = _mm256_load_pd(result);
+    __m256d r1 = _mm256_load_pd(result + 4);
+
+    for (unsigned int j = 0; j < size; j++) {
+        // 0,1,2,3
+        __m256d x = _mm256_add_pd(r0, d0);
+        __m256d nr = _mm256_cvtps_pd(_mm_rsqrt_ps(_mm256_cvtpd_ps(x))); // inverse sqrt approximate
+        __m256d muls = _mm256_mul_pd(_mm256_mul_pd(x, nr), nr);
+        nr = _mm256_mul_pd(_mm256_mul_pd(half, nr), _mm256_sub_pd(three, muls)); // newton iteration
+        r0 = _mm256_add_pd(r0, nr);
+        // 4,5,6,7
+        x = _mm256_add_pd(r1, d1);
+        nr = _mm256_cvtps_pd(_mm_rsqrt_ps(_mm256_cvtpd_ps(x))); // inverse sqrt approximate
+        muls = _mm256_mul_pd(_mm256_mul_pd(x, nr), nr);
+        nr = _mm256_mul_pd(_mm256_mul_pd(half, nr), _mm256_sub_pd(three, muls)); // newton iteration
+        r1 = _mm256_add_pd(r1, nr);
+    }
+    _mm256_store_pd(result, r0);
+    _mm256_store_pd(result + 4, r1);
+
+    timer.stop("avx rsqrt double NWTN 1 complete, ");
+    timer.dump();
+    double sum = 0;
+    for (int i = 0; i < 8; i++) {
+        sum += result[i];
+    }
+    std::cout << "result0: " << std::setprecision(std::numeric_limits<double>::digits10 + 2) << sum << ", ";
+    printf("Speed: %lf MRSQRT/s\n", (size * 8) * (1e-6) / timer.getTime());
+}
+
+void test_avxd2(int size) {
+    AlignedMemory<double, 32> datamem(8);
+    AlignedMemory<double, 32> resultmem(8);
+
+    double *data = datamem.alignedPtr;
+    double *result = resultmem.alignedPtr;
+
+    for (int i = 0; i < 8; i++) {
+        data[i] = i + 1;
+        result[i] = 0;
+    }
+    // float data[8] = {1, 2, 3, 4, 5, 6, 7, 8};
+    // float result[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+
+    Timer timer;
+    timer.start();
+
+    const __m256d d0 = _mm256_load_pd(data);
+    const __m256d d1 = _mm256_load_pd(data + 4);
+    const __m256d half = _mm256_set1_pd(0.5);
+    const __m256d three = _mm256_set1_pd(3.0);
+
+    __m256d r0 = _mm256_load_pd(result);
+    __m256d r1 = _mm256_load_pd(result + 4);
+
+    for (unsigned int j = 0; j < size; j++) {
+        // 0,1,2,3
+        __m256d x = _mm256_add_pd(r0, d0);
+        __m256d nr = _mm256_cvtps_pd(_mm_rsqrt_ps(_mm256_cvtpd_ps(x))); // inverse sqrt approximate
+        __m256d muls = _mm256_mul_pd(_mm256_mul_pd(x, nr), nr);
+        nr = _mm256_mul_pd(_mm256_mul_pd(half, nr), _mm256_sub_pd(three, muls)); // newton iteration
+        muls = _mm256_mul_pd(_mm256_mul_pd(x, nr), nr);
+        nr = _mm256_mul_pd(_mm256_mul_pd(half, nr), _mm256_sub_pd(three, muls)); // newton iteration
+        r0 = _mm256_add_pd(r0, nr);
+        // 4,5,6,7
+        x = _mm256_add_pd(r1, d1);
+        nr = _mm256_cvtps_pd(_mm_rsqrt_ps(_mm256_cvtpd_ps(x))); // inverse sqrt approximate
+        muls = _mm256_mul_pd(_mm256_mul_pd(x, nr), nr);
+        nr = _mm256_mul_pd(_mm256_mul_pd(half, nr), _mm256_sub_pd(three, muls)); // newton iteration
+        muls = _mm256_mul_pd(_mm256_mul_pd(x, nr), nr);
+        nr = _mm256_mul_pd(_mm256_mul_pd(half, nr), _mm256_sub_pd(three, muls)); // newton iteration
+        r1 = _mm256_add_pd(r1, nr);
+    }
+    _mm256_store_pd(result, r0);
+    _mm256_store_pd(result + 4, r1);
+
+    timer.stop("avx rsqrt double NWTN 2 complete, ");
+    timer.dump();
+    double sum = 0;
+    for (int i = 0; i < 8; i++) {
+        sum += result[i];
+    }
+    std::cout << "result0: " << std::setprecision(std::numeric_limits<double>::digits10 + 2) << sum << ", ";
+    printf("Speed: %lf MRSQRT/s\n", (size * 8) * (1e-6) / timer.getTime());
+}
+
+/***********************
+ *   Single Precision  *
+ ***********************/
+inline float frsqrtf(float number) {
+    union {
+        float f;
+        long i;
+    } conv;
+
+    float x2;
+    const float threehalfs = 1.5F;
+
+    x2 = number * 0.5F;
+    conv.f = number;
+    conv.i = 0x5f3759df - (conv.i >> 1); // magic number for float
+    conv.f = conv.f * (threehalfs - (x2 * conv.f * conv.f));
+    conv.f = conv.f * (threehalfs - (x2 * conv.f * conv.f));
+    return conv.f;
 }
 
 void test_simplef(int size) {
@@ -82,7 +223,30 @@ void test_simplef(int size) {
     for (int i = 0; i < 8; i++) {
         sum += result[i];
     }
-    printf("result0: %.17g; Speed: %lf MRSQRT/s\n", sum, (size * 8) * (1e-6) / timer.getTime());
+    std::cout << "result0: " << std::setprecision(std::numeric_limits<float>::digits10 + 2) << sum << ", ";
+    printf("Speed: %lf MRSQRT/s\n", (size * 8) * (1e-6) / timer.getTime());
+}
+
+void test_magicf(int size) {
+
+    float data[8] = {1, 2, 3, 4, 5, 6, 7, 8};
+    float result[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+
+    Timer timer;
+    timer.start();
+    for (int j = 0; j < size; j++) {
+        for (int i = 0; i < 8; i++) {
+            result[i] += frsqrtf(data[i] + result[i]);
+        }
+    }
+    timer.stop("FLOAT magic rsqrt complete, ");
+    timer.dump();
+    double sum = 0;
+    for (int i = 0; i < 8; i++) {
+        sum += result[i];
+    }
+    std::cout << "result0: " << std::setprecision(std::numeric_limits<float>::digits10 + 2) << sum << ", ";
+    printf("Speed: %lf MRSQRT/s\n", (size * 8) * (1e-6) / timer.getTime());
 }
 
 void test_avxf0(int size) {
@@ -124,7 +288,8 @@ void test_avxf0(int size) {
     for (int i = 0; i < 8; i++) {
         sum += result[i];
     }
-    printf("result0: %.17g; Speed: %lf MRSQRT/s\n", sum, (size * 8) * (1e-6) / timer.getTime());
+    std::cout << "result0: " << std::setprecision(std::numeric_limits<float>::digits10 + 2) << sum << ", ";
+    printf("Speed: %lf MRSQRT/s\n", (size * 8) * (1e-6) / timer.getTime());
 }
 
 void test_avxf1(int size) {
@@ -166,107 +331,9 @@ void test_avxf1(int size) {
     for (int i = 0; i < 8; i++) {
         sum += result[i];
     }
-    printf("result0: %.17g; Speed: %lf MRSQRT/s\n", sum, (size * 8) * (1e-6) / timer.getTime());
+    std::cout << "result0: " << std::setprecision(std::numeric_limits<float>::digits10 + 2) << sum << ", ";
+    printf("Speed: %lf MRSQRT/s\n", (size * 8) * (1e-6) / timer.getTime());
 }
-
-void test_avxd2(int size) {
-    AlignedMemory<double, 32> datamem(8);
-    AlignedMemory<double, 32> resultmem(8);
-
-    double *data = datamem.alignedPtr;
-    double *result = resultmem.alignedPtr;
-
-    for (int i = 0; i < 8; i++) {
-        data[i] = i + 1;
-        result[i] = 0;
-    }
-    // float data[8] = {1, 2, 3, 4, 5, 6, 7, 8};
-    // float result[8] = {0, 0, 0, 0, 0, 0, 0, 0};
-
-    Timer timer;
-    timer.start();
-
-    const __m256d d0 = _mm256_load_pd(data);
-    const __m256d d1 = _mm256_load_pd(data + 4);
-    const __m256d half = _mm256_set1_pd(0.5);
-    const __m256d three = _mm256_set1_pd(3.0);
-
-    __m256d r0 = _mm256_load_pd(result);
-    __m256d r1 = _mm256_load_pd(result + 4);
-
-    for (unsigned int j = 0; j < size; j++) {
-        // 0,1,2,3
-        __m256d x = _mm256_add_pd(r0, d0);
-        __m256d nr = _mm256_cvtps_pd(_mm_rsqrt_ps(_mm256_cvtpd_ps(x))); // inverse sqrt approximate
-        __m256d muls = _mm256_mul_pd(_mm256_mul_pd(x, nr), nr);
-        __m256d result = _mm256_mul_pd(_mm256_mul_pd(half, nr), _mm256_sub_pd(three, muls)); // newton iteration
-        muls = _mm256_mul_pd(_mm256_mul_pd(x, nr), nr);
-        result = _mm256_mul_pd(_mm256_mul_pd(half, nr), _mm256_sub_pd(three, muls)); // newton iteration
-        r0 = _mm256_add_pd(r0, result);
-        // 4,5,6,7
-        x = _mm256_add_pd(r1, d1);
-        nr = _mm256_cvtps_pd(_mm_rsqrt_ps(_mm256_cvtpd_ps(x))); // inverse sqrt approximate
-        muls = _mm256_mul_pd(_mm256_mul_pd(x, nr), nr);
-        result = _mm256_mul_pd(_mm256_mul_pd(half, nr), _mm256_sub_pd(three, muls)); // newton iteration
-        muls = _mm256_mul_pd(_mm256_mul_pd(x, nr), nr);
-        result = _mm256_mul_pd(_mm256_mul_pd(half, nr), _mm256_sub_pd(three, muls)); // newton iteration
-        r1 = _mm256_add_pd(r1, result);
-    }
-    _mm256_store_pd(result, r0);
-    _mm256_store_pd(result + 4, r1);
-
-    timer.stop("avx rsqrt double NWTN 2 complete, ");
-    timer.dump();
-    double sum = 0;
-    for (int i = 0; i < 8; i++) {
-        sum += result[i];
-    }
-    printf("result0: %.17g; Speed: %lf MRSQRT/s\n", sum, (size * 8) * (1e-6) / timer.getTime());
-}
-
-void test_magicf(int size) {
-
-    float data[8] = {1, 2, 3, 4, 5, 6, 7, 8};
-    float result[8] = {0, 0, 0, 0, 0, 0, 0, 0};
-
-    Timer timer;
-    timer.start();
-    for (int j = 0; j < size; j++) {
-        for (int i = 0; i < 8; i++) {
-            result[i] += frsqrtf(data[i] + result[i]);
-        }
-    }
-    timer.stop("FLOAT magic rsqrt complete, ");
-    timer.dump();
-    double sum = 0;
-    for (int i = 0; i < 8; i++) {
-        sum += result[i];
-    }
-    printf("result0: %.17g; Speed: %lf MRSQRT/s\n", sum, (size * 8) * (1e-6) / timer.getTime());
-}
-
-void test_magicd(int size) {
-
-    double data[8] = {1, 2, 3, 4, 5, 6, 7, 8};
-    double result[8] = {0, 0, 0, 0, 0, 0, 0, 0};
-
-    Timer timer;
-    timer.start();
-    for (int j = 0; j < size; j++) {
-        for (int i = 0; i < 8; i++) {
-            result[i] += frsqrtd(data[i] + result[i]);
-        }
-    }
-    timer.stop("DOUBLE magic rsqrt complete, ");
-    timer.dump();
-    double sum = 0;
-    for (int i = 0; i < 8; i++) {
-        sum += result[i];
-    }
-    printf("result0: %.17g; Speed: %lf MRSQRT/s\n", sum, (size * 8) * (1e-6) / timer.getTime());
-}
-
-void test_ompsimd(int size) {}
 
 int main() {
     constexpr int size = 1000000;
@@ -277,6 +344,7 @@ int main() {
     printf("**double precision results**\n");
     test_simple(size);
     test_magicd(size);
+    test_avxd1(size);
     test_avxd2(size);
     printf("------------------------\n");
 
